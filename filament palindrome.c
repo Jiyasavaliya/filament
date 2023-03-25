5bf4a686f183215155414b8f4e70d24823e08379
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-03-25 13:09:06 +0530

    Add files via upload

---
+++ filament palindrome.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    int i, len, sum = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    len = strlen(str);
+
+    for(i=0; i < len/2; i++)
+    {
+        if(str[i] != str[len-i-1])
+        {
+            sum = 1;
+            
+        }
+    }
+
+    if (sum == 1)
+    {
+        printf("%s is not a palindrome\n", str);
+    }
+    else
+    {
+        printf("%s is a palindrome\n", str);
+    }
+
+    return 0;
+}
+
